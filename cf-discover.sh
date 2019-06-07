0894a89475d2e03313519191a5c68dbe72b46f23
David Mathis <dbmathis@gmail.com>
2019-06-06 20:48:41 -0400

    work

--- cf-discover.sh
+++ cf-discover.sh
@@ -16,6 +16,11 @@ function load_all_pages {
 
 ORG_GUID=$(load_all_pages "/v3/organizations/" | jq -r ' .[] | select(.name == "dmathis") | .guid')
 
-load_all_pages "/v3/spaces?organization_guids=${ORG_GUID}" | jq -r ' .[].guid' | while read -r line ; do
-	echo "This is a guid $line";
+load_all_pages "/v3/tasks?organization_guids=${ORG_GUID}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "Org: dmathis is using %.0f MB\n\n", s}'
+
+load_all_pages "/v3/spaces?organization_guids=${ORG_GUID}" | jq -r ' .[].guid' | while read -r LINE ; do
+   SPACE_MEM=$(load_all_pages "/v3/tasks?space_guids=${LINE}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f MB\n", s}')
+   #echo $LINE
+   SPACE_NAME=$(cf curl "/v3/spaces/${LINE}" | jq .name)
+   echo "Space: ${SPACE_NAME} is using ${SPACE_MEM}";
 done
