bd86bf372fa6a01c85ed41a59d40977600270209
David Mathis <dbmathis@gmail.com>
2019-06-06 23:00:05 -0400

    worked on script

--- cf-discover.sh
+++ cf-discover.sh
@@ -12,15 +12,14 @@ function load_all_pages {
     echo "$DATA" | jq .[] | jq -s
 }
 
-# load_all_pages "/v3/tasks?states=RUNNING&organization_guids=0c2c4234-c66a-440a-8d54-1afffe9e1b18" 
-
 ORG_GUID=$(load_all_pages "/v3/organizations/" | jq -r ' .[] | select(.name == "dmathis") | .guid')
 
-load_all_pages "/v3/tasks?organization_guids=${ORG_GUID}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "Org: dmathis is using %.0f MB\n\n", s}'
+OUTPUT=$(load_all_pages "/v3/tasks?organization_guids=${ORG_GUID}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "Org dmathis tasks are using %.0f MB of memory.\n", s}')
 
-load_all_pages "/v3/spaces?organization_guids=${ORG_GUID}" | jq -r ' .[].guid' | while read -r LINE ; do
-   SPACE_MEM=$(load_all_pages "/v3/tasks?space_guids=${LINE}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f MB\n", s}')
-   #echo $LINE
-   SPACE_NAME=$(cf curl "/v3/spaces/${LINE}" | jq .name)
-   echo "Space: ${SPACE_NAME} is using ${SPACE_MEM}";
+for SPACE in $(load_all_pages "/v3/spaces?organization_guids=${ORG_GUID}" | jq -r ' .[].guid'); do
+   SPACE_MEM=$(load_all_pages "/v3/tasks?space_guids=${SPACE}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f MB\n", s}')
+   SPACE_NAME=$(cf curl "/v3/spaces/${SPACE}" | jq -r .name)
+   OUTPUT=$(echo -e "${OUTPUT} \nSpace ${SPACE_NAME} tasks are using ${SPACE_MEM} of memory.")    
 done
+
+echo "$OUTPUT"
