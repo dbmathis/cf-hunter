27b967612e79d99d482f0c5172a72d053574be55
David Mathis <dbmathis@gmail.com>
2019-06-07 22:47:09 -0400

    worked on script

--- cf-hunter.sh
+++ cf-hunter.sh
@@ -1,48 +1,62 @@
 #!/bin/bash
 
 function load_all_pages {
-    URL="$1"
-    DATA=""
-    until [ "$URL" == "null" ]; do
-        RESP=$(cf curl "$URL")
-        DATA+=$(echo "$RESP" | jq .resources)
-        URL=$(echo "$RESP" | jq -r .next_url)
+    url="$1"
+    data=""
+    until [ "$url" == "null" ]; do
+        resp=$(cf curl "$url")
+        data+=$(echo "$resp" | jq .resources)
+        url=$(echo "$resp" | jq -r .next_url)
     done
     # dump the data
-    echo "$DATA" | jq .[] | jq -s
+    echo "$data" | jq .[] | jq -s
 }
 
-ORG_GUID=$(load_all_pages "/v3/organizations/" | jq -r ' .[] | select(.name == "dmathis") | .guid')
+org_guid=$(load_all_pages "/v3/organizations/" | jq -r ' .[] | select(.name == "dmathis") | .guid')
 
-ORG_TASKS_TOTAL_MEM=$(load_all_pages "/v3/tasks?organization_guids=${ORG_GUID}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
-ORG_PROCESSES_TOTAL_MEM=$(load_all_pages "/v3/processes?organization_guids=${ORG_GUID}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
-ORG_TOTAL_MEM=$(($ORG_TASKS_TOTAL_MEM+$ORG_PROCESSES_TOTAL_MEM))
+org_tasks_total_mem=$(load_all_pages "/v3/tasks?organization_guids=${org_guid}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+org_processes_total_mem=$(load_all_pages "/v3/processes?organization_guids=${org_guid}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+org_total_mem=$(($org_tasks_total_mem+$org_processes_total_mem))
+org_tasks_total_disk=$(load_all_pages "/v3/tasks?organization_guids=${org_guid}" | jq -r ' .[].disk_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+org_processes_total_disk=$(load_all_pages "/v3/processes?organization_guids=${org_guid}" | jq -r ' .[].disk_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+org_total_disk=$(($org_tasks_total_disk+$org_processes_total_disk))
 
-echo "ORG_TASKS_TOTAL_MEM ${ORG_TASKS_TOTAL_MEM}"
-echo "ORG_PROCESSES_TOTAL_MEM ${ORG_PROCESSES_TOTAL_MEM}"
-echo "ORG_TOTAL_MEM ${ORG_TOTAL_MEM}"
+div1="printf '%80s\n' | tr ' ' ="
+div2="printf '%80s\n' | tr ' ' -"
 
-for SPACE in $(load_all_pages "/v3/spaces?organization_guids=${ORG_GUID}" | jq -r ' .[].guid'); do
-   SPACE_NAME=$(cf curl "/v3/spaces/${SPACE}" | jq -r .name)
-   SPACE_TASKS_TOTAL_MEM=$(load_all_pages "/v3/tasks?space_guids=${SPACE}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
-   SPACE_PROCESSES_TOTAL_MEM=$(load_all_pages "/v3/processes?space_guids=${SPACE}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
-   SPACE_TOTAL_MEM=$(($SPACE_TASKS_TOTAL_MEM+$SPACE_PROCESSES_TOTAL_MEM))
-   echo "   SPACE_NAME ${SPACE_NAME}"
-   echo "   SPACE_TASKS_TOTAL_MEM ${SPACE_TASKS_TOTAL_MEM}"
-   echo "   SPACE_PROCESSES_TOTAL_MEM ${SPACE_PROCESSES_TOTAL_MEM}"
-   echo "   SPACE_TOTAL_MEM ${SPACE_TOTAL_MEM}"
-   for APP in $(load_all_pages "/v3/apps?space_guids=${SPACE}" | jq -r ' .[].guid'); do
-      APP_NAME=$(cf curl "/v3/apps/${APP}" | jq -r .name)
-      echo "      APP_NAME ${APP_NAME}"
-      for TASK in $(load_all_pages "/v3/tasks?app_guids=${APP}" | jq -r ' .[].guid'); do
-         TASK_NAME=$(cf curl "/v3/tasks/${TASK}" | jq -r .name)
-         echo "         TASK_NAME ${TASK_NAME}"
+printf "%-50s%15s%15s\n" "Hierarchy" "Disk" "Memory" 
+eval "$div1"
+printf "%-50s%15s%15s\n" "Org: dmathis" "$org_total_disk MB" "$org_total_mem MB"
+
+for space in $(load_all_pages "/v3/spaces?organization_guids=${org_guid}" | jq -r ' .[].guid'); do
+   space_name=$(cf curl "/v3/spaces/${space}" | jq -r .name)
+   space_tasks_total_mem=$(load_all_pages "/v3/tasks?space_guids=${space}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+   space_processes_total_mem=$(load_all_pages "/v3/processes?space_guids=${space}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+   space_total_mem=$(($space_tasks_total_mem+$space_processes_total_mem))
+   space_tasks_total_disk=$(load_all_pages "/v3/tasks?space_guids=${space}" | jq -r ' .[].disk_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+   space_processes_total_disk=$(load_all_pages "/v3/processes?space_guids=${space}" | jq -r ' .[].disk_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+   space_total_disk=$(($space_tasks_total_disk+$space_processes_total_disk))
+   eval "$div1"
+   printf "%-5s%-45s%15s%15s\n" "" "Space :: $space_name" "$space_total_disk MB" "$space_total_mem MB"
+   for app in $(load_all_pages "/v3/apps?space_guids=${space}" | jq -r ' .[].guid'); do
+      app_name=$(cf curl "/v3/apps/${app}" | jq -r .name)
+      app_tasks_total_mem=$(load_all_pages "/v3/tasks?app_guids=${app}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+      app_processes_total_mem=$(load_all_pages "/v3/processes?app_guids=${app}" | jq -r ' .[].memory_in_mb' | awk '{s+=$1} END {printf "%.0f", s}')
+      app_total_mem=$(($app_tasks_total_mem+$app_processes_total_mem))
+      eval "$div2"      
+      printf "%-10s%-40s%30s\n" "" "App :: $app_name" "$app_total_mem MB"
+      eval "$div2"
+      for task in $(load_all_pages "/v3/tasks?app_guids=${app}" | jq -r ' .[].guid'); do
+         task_name=$(cf curl "/v3/tasks/${task}" | jq -r .name)
+         task_mem=$(cf curl "/v3/tasks/${task}" | jq -r .memory_in_mb)
+         printf "%-15s%-35s%30s\n" "" "Task :: $task_name" "$task_mem MB"
       done
-      for PROCESS in $(load_all_pages "/v3/processes?app_guids=${APP}" | jq -r ' .[].guid'); do
-         PROCESS_NAME=$(cf curl "/v3/processes/${PROCESS}" | jq -r .type)
-         echo "         PROCESS_NAME ${PROCESS_NAME}"
+      for process in $(load_all_pages "/v3/processes?app_guids=${app}" | jq -r ' .[].guid'); do
+         process_name=$(cf curl "/v3/processes/${process}" | jq -r .type)
+         process_mem=$(cf curl "/v3/processes/${process}" | jq -r .memory_in_mb)
+         printf "%-15s%-35s%30s\n" "" "Process :: $process_name" "$process_mem MB"
       done
    done  
 done
 
-echo "$OUTPUT"
+echo "$output"
